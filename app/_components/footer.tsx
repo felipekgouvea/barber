3ea5dc9c2cbc840bcf10b48161616243b25d0e07
felipekgouvea <felipekinupesg@hotmail.com>
2024-05-01 00:14:35 -0300

    feat: add Footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-secondary w-full px-5 py-6">
+      <p className="text-xs font-bold text-gray-400 opacity-75">
+        © 2023 Copyright FSW Barber
+      </p>
+    </div>
+  )
+}
+
+export default Footer
