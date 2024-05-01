1850493893ecda6560dd7a58e6eaca93b492d640
felipekgouvea <felipekinupesg@hotmail.com>
2024-04-30 22:27:59 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,21 @@
+import { ptBR } from 'date-fns/locale'
 import Header from '../_components/header'
+import { format } from 'date-fns'
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Header />
-    </>
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p className="text-sm capitalize">
+          {format(new Date(), "EEEE', ' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+    </div>
   )
 }
 
