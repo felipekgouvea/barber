05c6800c0e48f869077309acbd1145cf99692399
felipekgouvea <felipekinupesg@hotmail.com>
2024-05-02 20:48:19 -0300

    feat: ensure a service can be booked by the user

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -13,11 +13,13 @@ import {
 
 import { Barbershop, Service } from '@prisma/client'
 import { ptBR } from 'date-fns/locale'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import { useMemo, useState } from 'react'
 import { generateDayTimeList } from '../_helpers/hours'
-import { format } from 'date-fns'
+import { format, setHours, setMinutes } from 'date-fns'
+import { saveBooking } from '../_actions/save-booking'
+import { Loader2 } from 'lucide-react'
 
 interface ServiceitemProps {
   service: Service
@@ -30,8 +32,10 @@ const ServiceItem = ({
   barberShop,
   isAuthenticated,
 }: ServiceitemProps) => {
+  const { data } = useSession()
   const [date, setDate] = useState<Date | undefined>(undefined)
   const [hour, setHour] = useState<string | undefined>()
+  const [submitIsLoading, setSubmitIsLoading] = useState(false)
 
   const handleDateClick = (date: Date | undefined) => {
     setDate(date)
@@ -48,11 +52,35 @@ const ServiceItem = ({
     }
   }
 
+  const handleBookingSubmit = async () => {
+    setSubmitIsLoading(true)
+    try {
+      if (!hour || !date || !data?.user) {
+        return
+      }
+
+      const dateHour = Number(hour.split(':')[0])
+      const dateMinutes = Number(hour.split(':')[1])
+
+      const newDate = setMinutes(setHours(date, dateHour), dateMinutes)
+
+      await saveBooking({
+        serviceId: service.id,
+        barbershopId: barberShop.id,
+        date: newDate,
+        userId: (data?.user as any).id,
+      })
+      console.log('Entrou aqui')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setSubmitIsLoading(false)
+    }
+  }
+
   const timeList = useMemo(() => {
     return date ? generateDayTimeList(date) : []
   }, [date])
-
-  console.log(timeList)
 
   return (
     <Card>
@@ -178,7 +206,15 @@ const ServiceItem = ({
                   </div>
 
                   <SheetFooter className="px-5">
-                    <Button disabled={!hour || !date}>Confirmar reserva</Button>
+                    <Button
+                      onClick={handleBookingSubmit}
+                      disabled={!hour || !date || submitIsLoading}
+                    >
+                      {submitIsLoading && (
+                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                      )}
+                      Confirmar reserva
+                    </Button>
                   </SheetFooter>
                 </SheetContent>
               </Sheet>
